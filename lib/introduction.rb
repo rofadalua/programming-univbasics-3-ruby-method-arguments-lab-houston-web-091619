2625f1b806c02e6279178f4d2983482294b2a7ac
ultimate-bell-1709 <rofadalua@hotmail.com>
2019-09-12 02:01:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,11 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def
+#introduction ("Fabiola")
+
+{introduction_with_language("Fabiola") "(Ruby")}
+"Hi, my name is #{name} and I am learning to program in #{language}"
 
 
 
